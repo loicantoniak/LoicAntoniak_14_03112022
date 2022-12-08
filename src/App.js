3012c417f6980e4f1c03e7619f4ff12a0027d8bd
Antoniak Loïc <loic.antoniak@gmail.com>
2022-12-08 20:24:51 +0100

    disabled devtool in production

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { disableReactDevTools } from "@fvilers/disable-react-devtools"
 import React from "react"
 import { Provider } from "react-redux"
 import Navigation from "./pages/navigation/Navigation"
 import store from "./redux/store"
+
+// eslint-disable-next-line no-undef
+if(process.env.REACT_APP_NODE_ENV === "production") {
+  disableReactDevTools()
+}
 
 export default function App() {
   return (
